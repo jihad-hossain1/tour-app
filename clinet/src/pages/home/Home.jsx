ab470e16e06502fe395d05d1b902876c4e3bbb25
jihad-hossain1 <jihadkhan934@gmail.com>
2023-12-19 19:08:49 +0600

    some error fix on home page ui

--- clinet/src/pages/home/Home.jsx
+++ clinet/src/pages/home/Home.jsx
@@ -5,25 +5,27 @@ import { Container } from "@mui/material";
 
 const Home = () => {
   return (
-    <div>
+    <div className="pb-6">
       <div
-        className="bg-no-repeat bg-cover  w-full"
+        className="bg-no-repeat bg-cover  w-full "
         style={{
           backgroundImage:
             "url('https://res.cloudinary.com/dqfi9zw3e/image/upload/v1702948003/images/ya1b7b2wmtv6fmoi0fyl.webp')",
         }}
       >
-        <div className="pt-20">
+        <div className="pt-28 ">
           <div className="text-[#fff] text-center flex flex-col gap-5">
             <h4 className="font-extrabold text-6xl">Let the journey begin</h4>
             <p>Get the best prices on 2,000,000+ properties, worldwide</p>
           </div>
+        </div>
+        <div className="max-w-[1000px] mx-auto">
           <SearchTab />
         </div>
       </div>
       <Container maxWidth={"lg"}>
         {/* offers section  */}
-        <div>offer...</div>
+        {/* <div>offer...</div> */}
         {/* top destinations section  */}
         <HomeContinent />
         <div>{/* <Test /> */}</div>
