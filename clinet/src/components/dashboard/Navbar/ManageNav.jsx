4706c8799f5e2422e56400a0c50cdb24f613b9c7
jihad-hossain1 <jihadkhan934@gmail.com>
2023-12-26 18:32:48 +0600

    some chnge on dashboard

--- clinet/src/components/dashboard/Navbar/ManageNav.jsx
+++ clinet/src/components/dashboard/Navbar/ManageNav.jsx
@@ -1,6 +1,6 @@
 const ManageNav = ({ children }) => {
   return (
-    <div className="h-[35px] m-4 rounded border border-slate-200/70 bg-slate-100 shadow-[2px_4px_10px_rgba(0,0,0,0.25)]">
+    <div className="min-h-[25px] md:min-h-[35px] p-2 md:p-4 m-4 rounded border border-slate-200/70 bg-slate-100 shadow-[2px_3px_10px_rgba(0,0,0,0.25)] ">
       {children}
     </div>
   );
