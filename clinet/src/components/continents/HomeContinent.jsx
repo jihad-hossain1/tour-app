df81d14e727934a8fd89093033756534ab0a32d5
jihad-hossain1 <jihadkhan934@gmail.com>
2024-01-02 23:38:04 +0600

    tour by continents added on homepage

--- clinet/src/components/continents/HomeContinent.jsx
+++ clinet/src/components/continents/HomeContinent.jsx
@@ -20,23 +20,31 @@ const HomeContinent = () => {
   }
   return (
     <div className="mt-6 md:mt-10  px-2">
-      <h4 className="text-center my-3 md:my-16 font-bold text-3xl md:text-5xl ">
-        Top Destinations
-      </h4>
-      <div className="grid grid-cols-2 lg:grid-cols-3 md:gap-7 gap-5">
-        {data?.continents
+      <div className="my-6 lg:my-10">
+        <h4 className="text-3xl ">
+          <span className="border-b-[3px] pb-1 border-blue-600 w-fit">
+            Tour By
+          </span>{" "}
+          Continents
+        </h4>
+      </div>
+      <div className="grid grid-cols-2 lg:grid-cols-4 md:gap-7 gap-5">
+        {data?.continents?.map((continent, _i) => (
+          <SingleContinent key={_i} index={_i} continent={continent} />
+        ))}
+        {/* {data?.continents
           ?.slice(0, isToggle ? data?.continents?.length : 6)
           .map((continent, _i) => (
             <SingleContinent key={_i} index={_i} continent={continent} />
-          ))}
+          ))} */}
       </div>
-      <div className="mt-5">
+      {/* <div className="mt-5">
         {!isToggle ? (
           <button onClick={() => setIsToggle(!isToggle)}>Load more</button>
         ) : (
           <button onClick={() => setIsToggle(!isToggle)}>Show less</button>
         )}
-      </div>
+      </div> */}
     </div>
   );
 };
