8224f37ac8a7bc0e5f216a2e5047e0a4dbec0ccc
saifsammy <saifsammy653@gmail.com>
2024-01-16 22:53:36 +0600

    second header for test purpose

--- clinet/src/layout/Layout.jsx
+++ clinet/src/layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/Header/Header";
 import { Outlet } from "react-router-dom";
+import Header2 from "../components/Header/Header2";
 
 const Layout = () => {
   return (
     <div className="text-zinc-950 bg-zinc-50 w-full min-h-screen ">
       <div className="fixed z-50 w-full">
         <Header />
+        <Header2 />
       </div>
       <div className="">
         <Outlet />
