e479438a20b332e1b987fcc2ef52f8626a875115
saifsammy <saifsammy653@gmail.com>
2024-01-20 22:15:36 +0600

    pre Loader added

--- clinet/src/layout/Layout.jsx
+++ clinet/src/layout/Layout.jsx
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../components/Header/Header";
 import { Outlet } from "react-router-dom";
 import Header2 from "../components/Header/Header2";
 import Footer from "../components/Footer/Footer";
+import Loader from "./Loader/Loader";
 
 const Layout = () => {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setLoading(false)
+    }, 3000);
+    // Clean up the timeout to avoid memory leaks
+    return () => clearTimeout(timeoutId)
+  }, [])
   return (
-    <div className="text-zinc-950 bg-zinc-50 w-full min-h-screen ">
-      <div className="fixed z-50 w-full">
-        {/* <Header /> */}
-        <Header2 />
-      </div>
-      <div className="">
-        <Outlet />
-      </div>
-      <Footer />
-    </div>
+    <>
+      {loading ? (<Loader />) : (<div className="text-zinc-950 bg-zinc-50 w-full min-h-screen ">
+        <div className="fixed z-50 w-full">
+          {/* <Header /> */}
+          <Header2 />
+        </div >
+        <div className="">
+          <Outlet />
+        </div>
+        <Footer />
+      </div >)}
+    </>
   );
 };
 
