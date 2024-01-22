a3c7548ee4fc83f9c3dbaf0c96801464400c34ef
Nahidahmmed <nahidahmmed411@gmail.com>
2024-01-22 20:24:08 +0600

    Popular Tour Destinations

--- clinet/src/components/PopularDestination/PopularDestination.jsx
+++ clinet/src/components/PopularDestination/PopularDestination.jsx
@@ -1,4 +1,5 @@
 // import { Box, Card } from "@mui/material";
+import { Card } from "@mui/material";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 const countryData = [
@@ -37,7 +38,6 @@ const countryData = [
 ];
 
 const PopularDestination = () => {
-
   return (
     <main className="px-2">
       <div className="my-6 lg:my-10">
@@ -84,24 +84,28 @@ const PopularDestination = () => {
               </div>
             </Card> */}
 
-            <div className="relative rounded-lg overflow-hidden transition-transform transform hover:scale-105 duration-500 ease-in-out">
-              <div
-                className="w-full h-[210px] bg-cover bg-center pt-4 relative group"
-                style={{ backgroundImage: `url(${item.img})` }}
-              >
-                <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-30 transition-opacity opacity-0 group-hover:opacity-100">
-                  <div className="text-center w-full scale-0 translate-y-4 transition-transform duration-500 ease-in-out group-hover:scale-100 group-hover:translate-y-0">
-                    <button className="text-white text-xs bg-yellow-500 rounded-md w-[30%] mx-auto mb-4 h-[20px] transition-opacity duration-500 ease-in-out">
-                      Click to view
-                    </button>
-                    <h1 className="text-white text-3xl mx-auto">
-                      {item.name}
-                     
-                    </h1>
+            <Card>
+              <div className="relative rounded-lg overflow-hidden transition-transform transform hover:scale-105 duration-500 ease-in-out">
+                <div
+                  className="w-full h-[160px] bg-cover bg-center pt-4 relative group"
+                  style={{ backgroundImage: `url(${item.img})` }}
+                >
+                  <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-30 transition-opacity opacity-0 group-hover:opacity-100">
+                    <div className="text-center w-full scale-0 translate-y-4 transition-transform duration-500 ease-in-out group-hover:scale-100 group-hover:translate-y-0">
+                      <button className="text-white text-xs bg-yellow-500 rounded-md w-[30%] mx-auto mb-4 h-[20px] transition-opacity duration-500 ease-in-out">
+                        Click to view
+                      </button>
+                      <h1 className="text-white text-3xl mx-auto">
+                        {item.name}
+                      </h1>
+                    </div>
                   </div>
                 </div>
+                <div className="py-2 border-2">
+                  <h4 className="text-center font-semibold">{item.name}</h4>
+                </div>
               </div>
-            </div>
+            </Card>
           </Link>
         ))}
       </section>
