d4afd7574dd8635f3ecd40edde069e9d7d023da8
saifsammy <saifsammy653@gmail.com>
2024-02-01 20:24:58 +0600

    garbage code remove

--- clinet/src/pages/home/Home.jsx
+++ clinet/src/pages/home/Home.jsx
@@ -2,13 +2,13 @@ import Slider from "./SliderSection/Slider";
 import components from "../../components/AllImporter/HomeImporter/HomeImporter";
 const Home = () => {
   return (
-    <div>
+    <>
       <Slider></Slider>
       {components.map((compo, index) => (
         <div key={index} className="max-w-screen-xl mx-auto">
           {compo}
         </div>))}
-    </div>
+    </>
   );
 };
 
