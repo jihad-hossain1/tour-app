3dab8581481e4725e97928511fee6200dc787c21
saifsammy <saifsammy653@gmail.com>
2024-02-03 20:45:17 +0600

    popular virtual tours update

--- clinet/src/components/AllDemoDataImporter/AllDemoDataImporter.jsx
+++ clinet/src/components/AllDemoDataImporter/AllDemoDataImporter.jsx
@@ -284,6 +284,49 @@ export const latestArticles = [
     }
 ];
 
+export const popularVirtualTours = [
+    {
+        title: "Wine Tasting Experience",
+        city: "Barcelona",
+        price: 60,
+        duration: 3,
+        reviews: 110,
+        stars: 4.6,
+        description:
+            "Indulge in the flavors of Catalonia with a wine tasting experience in Barcelona, sampling exquisite local wines paired with regional delicacies.",
+    },
+    {
+        title: "Art Gallery Tour",
+        city: "Paris",
+        price: 45,
+        duration: 2,
+        reviews: 100,
+        stars: 4.7,
+        description:
+            "Immerse yourself in the world of art with a curated tour of Parisian art galleries, featuring masterpieces from renowned artists and emerging talents.",
+    },
+    {
+        title: "Scuba Diving Adventure",
+        city: "Bali",
+        price: 100,
+        duration: 5,
+        reviews: 75,
+        stars: 4.9,
+        description:
+            "Dive into the crystal-clear waters of Bali for an unforgettable scuba diving adventure, exploring vibrant coral reefs and encountering diverse marine life.",
+    },
+    {
+        title: "Concert Under the Stars",
+        city: "New York",
+        price: 75,
+        duration: 4.5,
+        reviews: 130,
+        stars: 4.8,
+        description:
+            "Experience the magic of live music under the stars in New York, with a concert featuring talented artists performing in an enchanting outdoor setting.",
+    },
+];
+
 export const countries = [
     { code: "AD", label: "Andorra", phone: "376" },
     {
